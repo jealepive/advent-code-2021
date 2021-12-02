797ea957ba38ec2b49014f349234642f676376d2
Jean Pico <jean.pico@eversource.com>
2021-12-01 22:32:47 -0500

    Day 1

---
+++ Day_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using Utils;
+
+string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+var inputList = ReadIndata.Ints(inputPath);
+
+//--- Day 1: Sonar Sweep ---
+
+void PartA()
+{
+    var count = GetIncreasedCount(inputList);
+
+    Console.WriteLine($"Part A: Result is {count}");
+}
+
+void PartB()
+{
+    var windowSum = new List<int>();
+
+    for (int i = 0; i < inputList.Count; i++)
+    {
+        if (i + 3 > inputList.Count)
+        {
+            break;
+        }
+        windowSum.Add(inputList[i] + inputList[i + 1] + inputList[i + 2]);
+    }
+    var count = GetIncreasedCount(windowSum);
+
+    Console.WriteLine($"Part B: Result is {count}");
+}
+
+int GetIncreasedCount(IReadOnlyList<int> list)
+{
+    var count = 0;
+    for (int i = 1; i < list.Count; i++)
+    {
+        if (list[i - 1] < list[i])
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+PartA();
+PartB();
---
+++ Utils/ReadIndata.cs
@@ -0,0 +1,41 @@
+﻿namespace Utils
+{
+    public static class ReadIndata
+    {
+        public static List<int> Ints(string path)
+        {
+            var streamReader = File.OpenText(path);
+            var list = new List<int>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                list.AddRange(line.Split(',').Select(int.Parse).ToList());
+            }
+            return list;
+        }
+
+        public static List<long> Longs(string path)
+        {
+            var streamReader = File.OpenText(path);
+            var list = new List<long>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                list.AddRange(line.Split(',').Select(long.Parse).ToList());
+            }
+            return list;
+        }
+
+        public static List<string> Strings(string path)
+        {
+            var streamReader = File.OpenText(path);
+            var list = new List<string>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                list.Add(line);
+            }
+            return list;
+        }
+    }
+}
