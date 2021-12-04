de4f16bdd0d23d882c2b6b4f4067d3ab8fcd6fb1
Jean Alejandro Pico <jealepive@hotmail.com>
2021-12-04 16:55:55 -0500

    Day 03 - Part A

---
+++ src/Day_03/Program.cs
@@ -0,0 +1,37 @@
+﻿using Utils;
+
+var inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+var rawInput = ReadData.AsString(inputPath);
+var diagnosticReport = rawInput.Select(x => x.ToCharArray());
+var bitsWidth = diagnosticReport.First().Length;
+
+//---Day 3: Binary Diagnostic ---
+
+void PartA()
+{
+    var onesCountPerBit = new int[bitsWidth];
+
+    for (int i = 0; i < diagnosticReport.Count(); i++)
+    {
+        for (int j = 0; j < bitsWidth; j++)
+        {
+            if (diagnosticReport.ElementAt(i).ElementAt(j) == '1')
+            {
+                onesCountPerBit[j]++;
+            }
+        }
+    }
+
+    var gammaRate = onesCountPerBit.Select(onesCount => onesCount > diagnosticReport.Count() / 2 ? '1' : '0');
+    var epsilonRate = onesCountPerBit.Select(onesCount => onesCount < diagnosticReport.Count() / 2 ? '1' : '0');
+    var powerConsumption = BinaryToDecimal(gammaRate) * BinaryToDecimal(epsilonRate);
+
+    Console.WriteLine($"Part A: Result is {powerConsumption}");
+}
+
+double BinaryToDecimal(IEnumerable<char> binaryNumber)
+{
+    return binaryNumber.Select((bit, idx) => bit == '1' ? Math.Pow(2, (binaryNumber.Count() - 1) - idx) : 0).Sum();
+}
+
+PartA();
---
+++ tests/Day_03/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Day_03
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
