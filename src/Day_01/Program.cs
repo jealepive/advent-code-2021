7a9b39544d524e16d3d1469b1fee33062e347a37
Jean Alejandro <jealepive@outlook.com>
2024-07-06 22:06:16 -0500

    Add code improvements to Day 01

--- src/Day_01/Program.cs
+++ src/Day_01/Program.cs
@@ -1,46 +1,48 @@
 ﻿using Utils;
 
-string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+var inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 var inputList = ReadData.AsInt(inputPath);
 
 //--- Day 1: Sonar Sweep ---
 
 void PartA()
 {
-    var count = GetIncreasedCount(inputList);
+	var count = GetIncreasedCount(inputList);
 
-    Console.WriteLine($"Part A: Result is {count}");
+	Console.WriteLine($"Part A: Result is {count}");
 }
 
 void PartB()
 {
-    var windowSum = new List<int>();
+	var windowSum = new List<int>();
 
-    for (int i = 0; i < inputList.Count; i++)
-    {
-        if (i + 3 > inputList.Count)
-        {
-            break;
-        }
-        windowSum.Add(inputList[i] + inputList[i + 1] + inputList[i + 2]);
-    }
-    var count = GetIncreasedCount(windowSum);
+	for (int i = 0; i < inputList.Count; i++)
+	{
+		if (i + 3 > inputList.Count)
+		{
+			break;
+		}
 
-    Console.WriteLine($"Part B: Result is {count}");
+		windowSum.Add(inputList[i] + inputList[i + 1] + inputList[i + 2]);
+	}
+
+	var count = GetIncreasedCount(windowSum);
+
+	Console.WriteLine($"Part B: Result is {count}");
 }
 
 int GetIncreasedCount(IReadOnlyList<int> list)
 {
-    var count = 0;
-    for (int i = 1; i < list.Count; i++)
-    {
-        if (list[i - 1] < list[i])
-        {
-            count++;
-        }
-    }
+	var count = 0;
+	for (int i = 1; i < list.Count; i++)
+	{
+		if (list[i - 1] < list[i])
+		{
+			count++;
+		}
+	}
 
-    return count;
+	return count;
 }
 
 PartA();
